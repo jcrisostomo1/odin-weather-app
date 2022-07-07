26c824345c3021a9450a424ed4ca2a9ed6456e40
jcrisostomo1 <joshuacr22@gmail.com>
2022-07-06 20:59:48 -0400

    save work

---
+++ myscript.js
@@ -0,0 +1,24 @@
+const l = document.querySelector("main-container");
+
+
+let div = document.createElement("h3");
+
+
+let kelvinToFahrenheit = (kelvin) => {
+    let fahrenheit = (9/5) * (kelvin-273) + 32;
+    return Math.round(fahrenheit);
+}
+
+
+let getWeather = async() => {
+    try {
+        const data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.347200&lon=-76.480720&appid=38a4497c5bdfd40114228ba9fcf7e3b8")
+        const weather = await data.json();
+        let kelvin = weather.main.temp;
+        console.log(kelvinToFahrenheit(kelvin));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getWeather();
