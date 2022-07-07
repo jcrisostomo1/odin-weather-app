cb0c0881fc7aa17e74a8ebad3de2eb805211c659
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-07 13:20:55 -0400

    save work

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 const input = document.querySelector("#search");
+const city = document.querySelector(".city");
 const temp = document.querySelector(".temp");
 
 
@@ -7,8 +8,9 @@ const temp = document.querySelector(".temp");
 
 
 class Weather {
-    constructor (city, temp, feels_like, humidity, pressure) {
+    constructor (city, country, temp, feels_like, humidity, pressure) {
         this.city = city;
+        this.country = country;
         this.temp = temp;
         this.feels_like = feels_like;
         this.humidity = humidity;
@@ -19,7 +21,8 @@ class Weather {
 input.addEventListener('keypress', async(e) => {
     if (e.key === 'Enter') {
         let weather = await getWeather();
-        temp.innerText += ` ${kelvinToFahrenheit(weather.temp)}`;
+        city.innerText = `${weather.city}, ${weather.country}`;
+        temp.innerText = ` ${kelvinToFahrenheit(weather.temp)}`;
     }
 });
 
@@ -34,10 +37,12 @@ let getWeather = async() => {
     try {
         const data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.347200&lon=-76.480720&appid=38a4497c5bdfd40114228ba9fcf7e3b8")
         const weather = await data.json();
-        let city = weather.name;
-        let objData = weather.main;
-        const { temp, feels_like, humidity, pressure } = objData;
-        let myWeather = new Weather(city, temp, feels_like, humidity, pressure);
+        console.log(weather);
+        const city = weather.name;
+        const country = weather.sys.country;
+        const { temp, feels_like, humidity, pressure } = weather.main;
+        let myWeather = new Weather(city, country, temp, feels_like, humidity, pressure);
+        console.log(myWeather)
         return myWeather;
     } catch (error) {
         console.log(error);
