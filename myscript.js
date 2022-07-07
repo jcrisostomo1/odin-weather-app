92fd2c417f10cb1a563216d5c1d6d4e199169013
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-07 16:18:23 -0400

    save work

--- myscript.js
+++ myscript.js
@@ -1,29 +1,29 @@
 const input = document.querySelector("#search");
+const searchButton = document.getElementById("search-btn");
 const city = document.querySelector(".city");
+const date = document.querySelector(".date");
 const temp = document.querySelector(".temp");
 
-
-
-// temp.innerText += 
-
-
 class Weather {
-    constructor (city, country, temp, feels_like, humidity, pressure) {
+    constructor (city, country, temp, feels_like, humidity, pressure, temp_max, temp_min) {
         this.city = city;
         this.country = country;
         this.temp = temp;
         this.feels_like = feels_like;
         this.humidity = humidity;
         this.pressure = pressure;
+        this.temp_max = temp_max,
+        this.temp_min = temp_min;
     } 
 }
 
-input.addEventListener('keypress', async(e) => {
-    if (e.key === 'Enter') {
-        let weather = await getWeather();
-        city.innerText = `${weather.city}, ${weather.country}`;
-        temp.innerText = ` ${kelvinToFahrenheit(weather.temp)}`;
-    }
+searchButton.addEventListener('click', async() => {
+    let inputCity = input.value;
+    let weather = await getWeather(inputCity);
+    city.innerText = `${weather.city}, ${weather.country}`;
+    date.innerText = new Date().toLocaleTimeString();
+    temp.innerText = ` ${kelvinToFahrenheit(weather.temp)}`;
+    
 });
 
 
@@ -33,16 +33,17 @@ let kelvinToFahrenheit = (kelvin) => {
 }
 
 
-let getWeather = async() => {
+let getWeather = async(city) => {
     try {
-        const data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.347200&lon=-76.480720&appid=38a4497c5bdfd40114228ba9fcf7e3b8")
+        const location = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=38a4497c5bdfd40114228ba9fcf7e3b8`);
+        console.log(location)
+        const data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.347200&lon=-76.480720&appid=38a4497c5bdfd40114228ba9fcf7e3b8");
         const weather = await data.json();
         console.log(weather);
         const city = weather.name;
         const country = weather.sys.country;
-        const { temp, feels_like, humidity, pressure } = weather.main;
-        let myWeather = new Weather(city, country, temp, feels_like, humidity, pressure);
-        console.log(myWeather)
+        const { temp, feels_like, humidity, pressure, temp_max, temp_min } = weather.main;
+        let myWeather = new Weather(city, country, temp, feels_like, humidity, pressure, temp_max, temp_min);
         return myWeather;
     } catch (error) {
         console.log(error);
